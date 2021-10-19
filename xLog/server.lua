6a2ed4596636f8c96b12d6ce1ac85bb2de11bae1
foroughinia18 <85922868+foroughinia18@users.noreply.github.com>
2021-10-19 14:44:26 +0330

    Update server.lua

--- xLog/server.lua
+++ xLog/server.lua
@@ -7,34 +7,34 @@ local function OnPlayerConnecting(name, setKickReason, deferrals)
 	local license = 'license:'
 	local discord = 'discord:'
 	local ip = ''
-    name = GetPlayerName(source)
-    for k, v in ipairs(GetPlayerIdentifiers(source)) do
-        if string.sub(v, 1, string.len('steam:')) == 'steam:' then
-            steam = v
-        end
-        if string.sub(v, 1, string.len('license:')) == 'license:' then
-            license = v
-        end
-        if string.sub(v, 1, string.len('discord:')) == 'discord:' then
-            discord = v
-        end
-    end
-    ip = GetPlayerEP(source)
+	name = GetPlayerName(source)
+	for k, v in ipairs(GetPlayerIdentifiers(source)) do
+		if string.sub(v, 1, string.len('steam:')) == 'steam:' then
+			steam = v
+		end
+		if string.sub(v, 1, string.len('license:')) == 'license:' then
+			license = v
+		end
+		if string.sub(v, 1, string.len('discord:')) == 'discord:' then
+			discord = v
+		end
+	end
+	ip = GetPlayerEP(source)
 	if name == nil then
-        name = ''
-    end
-    if steam == nil then
-        steam = 'steam:'
-    end
-    if license == nil then
-        license = 'license:'
-    end
-    if discord == nil then
-        discord = 'discord:'
-    end
-    if ip == nil then
-        ip = ''
-    end
+		name = ''
+	end
+	if steam == nil then
+		steam = 'steam:'
+	end
+	if license == nil then
+		license = 'license:'
+	end
+	if discord == nil then
+		discord = 'discord:'
+	end
+	if ip == nil then
+		ip = ''
+	end
 	if xlogwebhook ~= 'none' then
 		PerformHttpRequest(xlogwebhook, function(err, text, headers) end, 'POST', json.encode({content = '**Player Connecting**```name:'..name..'\n'..steam..'\n'..license..'\n'..discord..'\nip:'..ip..'```'}), { ['Content-Type'] = 'application/json' })
 	end
