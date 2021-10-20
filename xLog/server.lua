f5a4f343fe15647ffaf735b71317c6d52d95426b
foroughinia18 <85922868+foroughinia18@users.noreply.github.com>
2021-10-20 10:17:52 +0330

    Update server.lua

--- xLog/server.lua
+++ xLog/server.lua
@@ -1,6 +1,6 @@
 -- base
 local xlogwebhook = GetConvar('xlogwebhook', 'none')
--- log
+-- connect
 local function OnPlayerConnecting(name, setKickReason, deferrals)
 	local name = ''
 	local steam = 'steam:'
@@ -40,6 +40,20 @@ local function OnPlayerConnecting(name, setKickReason, deferrals)
 	end
 end
 AddEventHandler('playerConnecting', OnPlayerConnecting)
+-- chat
+AddEventHandler('chatMessage', function(source, author, message)
+	local name = GetPlayerName(source)
+	if name == nil then
+		name = ''
+	end
+	local ip = GetPlayerEP(source)
+	if ip == nil then
+		ip = ''
+	end
+	if xlogwebhook ~= 'none' then
+		PerformHttpRequest(xlogwebhook, function(err, text, headers) end, 'POST', json.encode({content = '**Player Chatting**```name:'..name..'\nmessage:'..message..'\nip:'..ip..'```'}), { ['Content-Type'] = 'application/json' })
+	end
+end)
 -- resource name
 Citizen.CreateThread(function()
 	while true do
