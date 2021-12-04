c5bdd62663b9faf9a95ce8d451cd4572bfd2ee7a
foroughinia18 <85922868+foroughinia18@users.noreply.github.com>
2021-12-04 10:25:54 +0330

    Update fxmanifest.lua

--- xLog/fxmanifest.lua
+++ xLog/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'foroughinia18#1533'
-description 'xLog'
+description 'xLog - FiveM log system based by Discord'
 version '1.0'
 
 server_script 'server.lua'
